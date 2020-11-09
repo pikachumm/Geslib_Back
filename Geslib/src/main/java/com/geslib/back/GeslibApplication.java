f51f1e98f789c7acb44e4dfbc26a7b8f5927845a
pikachumm <32273853+pikachumm@users.noreply.github.com>
2020-11-09 19:00:00 +0100

    creado proyecto inicial con lo basico y el conector a bbdd

---
+++ Geslib/src/main/java/com/geslib/back/GeslibApplication.java
@@ -0,0 +1,17 @@
+package com.geslib.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeslibApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		SpringApplication.run(GeslibApplication.class, args);
+	
+	}
+
+}
---
+++ Geslib/src/test/java/com/geslib/back/GeslibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geslib.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeslibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
